7222f6ed804375f007c5e95fe3bfbbe0e66b7652
Jérémy Trigano <trigano.jeremy@gmail.com>
2020-10-18 11:51:09 +0200

    modify readme for angular use

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 # modal-calculator
 
 For local use, to avoid CORS policy problem: run on local server (with http-server for example `npm install -g http-server`)
+
+## Angular use
+
+Go to your Angular project directory
+
+Type `npm i modal-calculator` to install modal-calculator package
+
+In your component.html add:
+
+```
+<div id="container">
+  <button id="calcBtn">Open</button>
+  <p>Click "open" button to open the modal calculator.</p>
+
+  <div id="calculator"></div>
+</div>
+```
+
+In your component.ts add:
+
+```
+import {ViewEncapsulation} from '@angular/core';
+
+@Component({
+  ...
+  styleUrls: [
+    ...
+    '../../node_modules/modal-calculator/style.css',
+  ],
+  encapsulation: ViewEncapsulation.None,
+})
+```
+
+The path `'../../node_modules/modal-calculator/style.css'` may be different, just point to node_modules directory.
+
+Then load script after init:
+`import { AfterViewInit } from '@angular/core';`
+`export class YourComponent implements AfterViewInit {...`
+
+```
+ngAfterViewInit() {
+  import('modal-calculator/index.js');
+}
+```
--- index.js
+++ index.js
@@ -1,15 +1,127 @@
 {
-  let calcDial = document.getElementById('calculator');
   let subText = '';
   let mainText = '';
+  let calcSelector = document.getElementById('calculator');
+  if (calcSelector) {
+    initCalc();
+    let calcBtn = document.getElementById('calcBtn');
+    calcSelector.style.display = 'none';
+    calcBtn.addEventListener('click', showCalc);
+    clickEvent();
+  }
+  bindFunctionsToKeys();
 
-  function showDialog() {
-    calcDial.show();
+  function initCalc() {
+    calcSelector.innerHTML = `      <div id="base">
+        <a id="closeBtn"></a>
+        <div id="mainDisplay">
+          <div id="subDisplay"><span id="subText"></span></div>
+          <span id="mainText"></span>
+        </div>
+        <div id="keypad">
+          <div id="grid-container">
+            <div class="keys" id="clearBtn">C</div>
+            <div class="keys opkey" id="backspaceBtn">&lt;=</div>
+            <div class="keys opkey" id="modBtn">%</div>
+            <div class="keys opkey" id="divideBtn">/</div>
+            <div class="keys opkey" id="multiplyBtn">*</div>
+            <div class="keys opkey" id="substractBtn">-</div>
+            <div class="keys opkey" id="addBtn">+</div>
+            <div class="keys opkey" id="equalBtn">=</div>
+            <div class="keys opkey" id="oppositeBtn">+/-</div>
+            <div class="keys opkey" id="dotBtn">.</div>
+            <div class="keys numkey" id="zeroBtn">0</div>
+            <div class="keys numkey" id="oneBtn">1</div>
+            <div class="keys numkey" id="twoBtn">2</div>
+            <div class="keys numkey" id="threeBtn">3</div>
+            <div class="keys numkey" id="fourBtn">4</div>
+            <div class="keys numkey" id="fiveBtn">5</div>
+            <div class="keys numkey" id="sixBtn">6</div>
+            <div class="keys numkey" id="sevenBtn">7</div>
+            <div class="keys numkey" id="eightBtn">8</div>
+            <div class="keys numkey" id="nineBtn">9</div>
+          </div>
+        </div>
+      </div>`;
   }
 
-  function closeDialog() {
+  function bindFunctionsToKeys() {
+    document.getElementById('closeBtn').addEventListener('click', closeCalc);
+    document.getElementById('clearBtn').addEventListener('click', allClear);
+    document
+      .getElementById('backspaceBtn')
+      .addEventListener('click', backspace);
+    document.getElementById('equalBtn').addEventListener('click', equal);
+    document.getElementById('oppositeBtn').addEventListener('click', opposite);
+    document.getElementById('zeroBtn').addEventListener('click', function () {
+      pressKey('0');
+    });
+    document.getElementById('oneBtn').addEventListener('click', function () {
+      pressKey('1');
+    });
+    document.getElementById('twoBtn').addEventListener('click', function () {
+      pressKey('2');
+    });
+    document.getElementById('threeBtn').addEventListener('click', function () {
+      pressKey('3');
+    });
+    document.getElementById('fourBtn').addEventListener('click', function () {
+      pressKey('4');
+    });
+    document.getElementById('fiveBtn').addEventListener('click', function () {
+      pressKey('5');
+    });
+    document.getElementById('sixBtn').addEventListener('click', function () {
+      pressKey('6');
+    });
+    document.getElementById('sevenBtn').addEventListener('click', function () {
+      pressKey('7');
+    });
+    document.getElementById('eightBtn').addEventListener('click', function () {
+      pressKey('8');
+    });
+    document.getElementById('nineBtn').addEventListener('click', function () {
+      pressKey('9');
+    });
+    document.getElementById('modBtn').addEventListener('click', function () {
+      pressKey('%');
+    });
+    document.getElementById('divideBtn').addEventListener('click', function () {
+      pressKey('/');
+    });
+    document
+      .getElementById('multiplyBtn')
+      .addEventListener('click', function () {
+        pressKey('*');
+      });
+    document
+      .getElementById('substractBtn')
+      .addEventListener('click', function () {
+        pressKey('-');
+      });
+    document.getElementById('addBtn').addEventListener('click', function () {
+      pressKey('+');
+    });
+    document.getElementById('dotBtn').addEventListener('click', function () {
+      pressKey('.');
+    });
+  }
+
+  function clickEvent() {
+    document.onclick = function (e) {
+      if (!e.path.includes(calcBtn) && !e.path.includes(calcSelector)) {
+        closeCalc();
+      }
+    };
+  }
+
+  function showCalc() {
+    calcSelector.style.display = 'block';
+  }
+
+  function closeCalc() {
     allClear();
-    calcDial.close();
+    calcSelector.style.display = 'none';
   }
 
   function displayMainText() {
